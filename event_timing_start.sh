a8644c75fc58fb504887474178bde77714bf9de2
unkoe <1789057425@qq.com>
2021-07-08 17:05:02 +0800

    covid-19简报更新  ---> update time: 2021-07-08 17:05:02

--- event_timing_start.sh
+++ event_timing_start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo "UPDATE CODES, "  pwd
+echo "UPDATE CODES, `pwd`"
 git pull
 echo "START TIMING"
 python3 main.py
-curr_pwd = `pwd`
-corntab * * * * * sh $curr_pwd/event_timing_start.sh
-
