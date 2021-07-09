c338e34508e510486f6ba5a62a0b692d13672352
unkoe <1789057425@qq.com>
2021-07-09 14:25:37 +0800

    test: url

--- event_timing_start.sh
+++ event_timing_start.sh
@@ -4,7 +4,6 @@
 cd /root/docker/Covid19Briefing
 git config --global user.email "***REMOVED***"
 git config --global user.name "unkoe"
-git config --global http.proxy socks5://danted:imH8f6fzosB80@138.128.214.84:1080
 echo "UPDATE ... `pwd`"
 /usr/bin/git pull
 echo "START TIMING"
