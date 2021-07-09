ad5f7e76b7c8c04bf1c4c04f16046be5e4f9ced5
unkoe <1789057425@qq.com>
2021-07-09 13:26:25 +0800

    feat: add logs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 import os_util
 import timing_push_repo
-
+import logging
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -17,5 +17,6 @@ src_file = "/root/docker/qq-robot/download/pipline/covid/covid-19.json"
 dest_file = os.getcwd() + "/covid19/covid-19.json"
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    logging.basicConfig(filename='log/debug.log', level=logging.INFO)
     os_util.copy(src_file, dest_file)
     timing_push_repo.start(dest_file)
