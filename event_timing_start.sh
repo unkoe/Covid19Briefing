913c98d198030bcc009b4cf904c34f463e40c9bf
unkoe <1789057425@qq.com>
2021-07-09 14:14:01 +0800

    Covid-19 简报更新  ---> update time: 2021-07-09 14:14:01

--- event_timing_start.sh
+++ event_timing_start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 . /etc/profile
-
+cd /root/docker/Covid19Briefing
+git config --global user.email "***REMOVED***"
+git config --global user.name "unkoe"
+git config --global http.proxy socks5://danted:imH8f6fzosB80@138.128.214.84:1080
 echo "UPDATE ... `pwd`"
 /usr/bin/git pull
 echo "START TIMING"
-/usr/bin/python3 /root/docker/Covid19Briefing/main.py
+/usr/bin/python3 main.py
