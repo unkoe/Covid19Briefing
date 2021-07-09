01b2f88d3a4c1614c3ee8c7b62d536493c378d72
unkoe <1789057425@qq.com>
2021-07-09 13:43:37 +0800

    feat: logs url is fail

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ src_file = "/root/docker/qq-robot/download/pipline/covid/covid-19.json"
 dest_file = os.getcwd() + "/covid19/covid-19.json"
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    logging.basicConfig(filename='log/debug.log',
+    logging.basicConfig(filename=os.getcwd()+'/log/debug.log',
                         level=logging.INFO,
-                        filemode='a',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
+                        filemode='a',
                         format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
     os_util.copy(src_file, dest_file)
     timing_push_repo.start(dest_file)
